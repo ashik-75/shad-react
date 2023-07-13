d4ba1f430b78534aa03a3126c56a29064cf202ef
Ashik <ashik@pop-os.localdomain>
2023-07-13 16:22:15 +0600

    add huge

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
+
+function App() {
+  return (
+    <>
+      <Alert>
+        <AlertTitle>Alert</AlertTitle>
+        <AlertDescription>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea quae quod
+          debitis illo quasi eum aperiam tenetur aut assumenda eius!
+        </AlertDescription>
+      </Alert>
+    </>
+  );
+}
+
+export default App;
